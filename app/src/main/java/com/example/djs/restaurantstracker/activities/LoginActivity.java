4139c6dcd17a5f2256b5086136d6b1419e4fafc6
SB <sbrandys13@gmail.com>
2016-03-12 17:17:56 +0100

    some test changes, added MainActivity

--- app/src/main/java/com/example/djs/restaurantstracker/activities/LoginActivity.java
+++ app/src/main/java/com/example/djs/restaurantstracker/activities/LoginActivity.java
@@ -1,15 +1,75 @@
 package com.example.djs.restaurantstracker.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.djs.restaurantstracker.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    @Bind(R.id.login_button)
+    LoginButton loginButton;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        initFacebookLogin();
+    }
+
+    private void initFacebookLogin() {
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Profile profile = Profile.getCurrentProfile();
+                AccessToken token = AccessToken.getCurrentAccessToken();
+
+                Log.d(TAG, "facebook login success, token: " + token.getToken() + ", uid: " + token.getUserId());
+                Log.d(TAG, "facebook login success, id: " + profile.getId() + ", name: " + profile.getFirstName() +
+                        ", surname: " + profile.getLastName());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook login error: " + error.getMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/example/djs/restaurantstracker/activities/MainActivityFragment.java
@@ -0,0 +1,24 @@
+package com.example.djs.restaurantstracker.activities;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.djs.restaurantstracker.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
